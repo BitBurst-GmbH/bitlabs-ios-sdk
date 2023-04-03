19f01f2f5095f2337b45e8232eef85e4eecf51f3
omaRaad <omar.raad@bitburst.net>
2023-04-03 14:55:30 +0200

    Add Trophy image
    Related issues: BLD-1372

--- BitLabs/Classes/Core/Views/LeaderboardRankingCell.swift
+++ BitLabs/Classes/Core/Views/LeaderboardRankingCell.swift
@@ -13,12 +13,16 @@ import Foundation
     @IBOutlet weak var usernameLabel: UILabel?
     @IBOutlet weak var topLabel: UILabel?
     @IBOutlet weak var rewardLabel: UILabel?
-    @IBOutlet weak var currencyImageView: UIImageView?
+    @IBOutlet weak var currencyIV: UIImageView?
+    @IBOutlet weak var trophyIV: UIImageView!
     
     @IBInspectable
     var rank: Int = 0 { didSet {
         rankLabel?.text = String(rank)
-        topLabel?.text = rank < 4 ? String(rank) : ""
+        let isTop3 = rank < 4
+        topLabel?.text = isTop3 ? String(rank) : ""
+        trophyIV?.isHidden = !isTop3
+        trophyIV.setImageColor(color: .blue)
     }}
     
     @IBInspectable
@@ -33,6 +37,6 @@ import Foundation
     
     @IBInspectable
     var currencyIcon: UIImage? = nil { didSet {
-        currencyImageView?.image = currencyIcon
+        currencyIV?.image = currencyIcon
     }}
 }
