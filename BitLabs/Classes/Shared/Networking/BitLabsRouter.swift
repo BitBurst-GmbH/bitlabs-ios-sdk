516ea4df4b75be4d4e06cd376e133e13d505f741
omaRaad <omar.raad@bitburst.net>
2023-06-28 12:25:31 +0200

    Migrate getOffers to v2
    Related issues: PRO-327

--- BitLabs/Classes/Shared/Networking/BitLabsRouter.swift
+++ BitLabs/Classes/Shared/Networking/BitLabsRouter.swift
@@ -26,7 +26,7 @@ enum BitLabsRouter {
         case .getSurveys:
             return "v2/client/surveys"
         case .getOffers:
-            return "v1/client/offers"
+            return "v2/client/offers"
         case .getAppSettings:
             return "v1/client/settings/v2"
         case .getLeaderboard:
@@ -51,7 +51,7 @@ enum BitLabsRouter {
         case .getSurveys(let sdk):
             return ["platform": getPlatform(), "os": "ios", "sdk": sdk]
         case .getOffers:
-            return ["platform": getPlatform()]
+            return ["platform": getPlatform(), "debug": true]
         case .getAppSettings:
             return ["platform": getPlatform()]
         case .getLeaderboard:
@@ -66,14 +66,13 @@ extension BitLabsRouter: URLRequestConvertible {
 		let url = try baseURL.asURL().appendingPathComponent(path)
 		var request = URLRequest(url: url)
 		request.method = method
-		
-        print(url)
-        
+		        
 		if method == .get {
             let queryItems = parameters.map { URLQueryItem(name: $0, value: "\($1)") }
             var urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false)
             urlComponents?.queryItems = queryItems
             request.url = urlComponents?.url
+            print(request.url)
 		} else if method == .post {
             request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: [])
 			request.setValue("application/json", forHTTPHeaderField: "Accept")
