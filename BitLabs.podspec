3dd2f1cf64d6d04018acd9a75540794e0ddbfab4
Omar <omar.raad@prodege.com>
2024-08-16 10:49:02 +0200

    Replace OHHTTPStubs to Mocker
    Related issue: PRO-1419

--- BitLabs.podspec
+++ BitLabs.podspec
@@ -39,10 +39,10 @@ Pod::Spec.new do |spec|
 #        host.resources = 'BitLabs/AppHost/**/*.{xib,storyboard}'
 #    end
     
-#    spec.test_spec 'UnitTests' do |test_spec|
-#      test_spec.source_files = 'BitLabs/Tests/Unit\ Tests/*.swift'
-#      test_spec.dependency 'OHHTTPStubs/Swift'
-#    end
+    spec.test_spec 'UnitTests' do |test_spec|
+      test_spec.source_files = 'BitLabs/Tests/Unit\ Tests/*.swift'
+      test_spec.dependency 'Mocker'
+    end
     
 #    spec.test_spec 'UITests' do |test_spec|
 #        test_spec.test_type = :ui
--- BitLabs/Tests/Unit Tests/BitLabsAPITest.swift
+++ BitLabs/Tests/Unit Tests/BitLabsAPITest.swift
@@ -7,30 +7,36 @@
 
 @testable import Alamofire
 @testable import BitLabs
-import OHHTTPStubs
 import Foundation
 import XCTest
-
-func forAll() -> HTTPStubsTestBlock {
-  return { _ in  return true }
-}
+import Mocker
 
 class BitLabsAPITest: XCTestCase {
     
+    var session: Session!
+    var bitlabsAPI: BitLabsAPI!
     var encoder = JSONEncoder() { didSet { encoder.keyEncodingStrategy = .convertToSnakeCase } }
     
-    override class func tearDown() {
-        HTTPStubs.removeAllStubs()
+    override func setUp() {
+        super.setUp()
+        let configuration = URLSessionConfiguration.af.default
+        configuration.protocolClasses = [MockingURLProtocol.self]
+        session = Session(configuration: configuration)
+        bitlabsAPI = BitLabsAPI(session)
+    }
+    
+    override func tearDown() {
+        Mocker.removeAll()
+        bitlabsAPI = nil
+        session = nil
         super.tearDown()
     }
     
     func testGetSurveys_Failure() {
-        stub(condition: forAll()) {_ in
-         return HTTPStubsResponse(error: Exception("Failure Error"))
-        }
-
-        let bitlabsAPI = BitLabsAPI(Session())
-
+        let url = BitLabsRouter.getSurveys(sdk: "").urlRequest!.url!
+        let mock = Mock(url: url, ignoreQuery: true, dataType: .json, statusCode: 500, data: [.get: Data()], requestError: Exception("Error"))
+        mock.register()
+        
         let expectation = XCTestExpectation(description: "Error received")
         bitlabsAPI.getSurveys(sdk: "") { result in
             switch result {
@@ -40,183 +46,189 @@ class BitLabsAPITest: XCTestCase {
                 expectation.fulfill()
             }
         }
-
+        
         wait(for: [expectation], timeout: 10.0)
     }
     
     func testGetSurveys_Response_Success() {
-        do {
-            let json = try encoder.encode(BitLabsResponse(data: GetSurveysResponse(restrictionReason: nil, surveys: [Survey(id: "", type: "", clickUrl: "", cpi: "", value: "", loi: 0.0, country: "", language: "", rating: 0, category: Category(name: "", iconUrl: "", iconName: "", nameInternal: ""), tags: [])]), error: nil, status: "", traceId: ""))
-                        
-            stub(condition: forAll()) { _ in
-                return HTTPStubsResponse(data: json, statusCode: Int32(200), headers: ["Content-Type": "application/json"])
+        let json = try! encoder.encode(
+            BitLabsResponse(
+                data: GetSurveysResponse(restrictionReason: nil, surveys: [Survey(id: "1", type: "", clickUrl: "", cpi: "", value: "", loi: 0.0, country: "", language: "", rating: 0, category: Category(name: "", iconUrl: "", iconName: "", nameInternal: ""), tags: [])]),
+                error: nil,
+                status: "",
+                traceId: ""
+            )
+        )
+        
+        let url = BitLabsRouter.getSurveys(sdk: "").urlRequest!.url!
+        let mock = Mock(url: url, ignoreQuery: true, dataType: .json, statusCode: 200, data: [.get: json])
+        mock.register()
+        
+        let expectation = XCTestExpectation(description: "Response received")
+        bitlabsAPI.getSurveys(sdk: "") { result in
+            switch result {
+            case .success(let surveys):
+                XCTAssertTrue(surveys.first?.id == "1")
+                expectation.fulfill()
+            case .failure(let error):
+                XCTFail("Request failed with error: \(error.localizedDescription)")
             }
-            
-            let bitlabsAPI = BitLabsAPI(Session())
-            let expectation = XCTestExpectation(description: "Response received")
-            
-            bitlabsAPI.getSurveys(sdk: "") { result in
-                switch result {
-                case .success(_):
-                    expectation.fulfill()
-                case .failure(let error):
-                    XCTFail("Request failed with error: \(error)")
-                }
-            }
-            
-            wait(for: [expectation], timeout: 10.0)
-        } catch(let error) {
-            XCTFail("Failed to parse hardcoded. Error: \(error)")
         }
+        
+        wait(for: [expectation], timeout: 10.0)
     }
     
     func testGetSurveys_Response_Error() {
-        do {
-            let json = try encoder.encode(BitLabsResponse<GetSurveysResponse>(data: nil, error: ErrorResponse(details: ErrorDetails(http: "400", msg: "Response Error")), status: "", traceId: ""))
-            
-            stub(condition: forAll()) { _ in
-                return HTTPStubsResponse(data: json, statusCode: Int32(200), headers: ["Content-Type": "application/json"])
+        let json = try! encoder.encode(
+            BitLabsResponse<GetSurveysResponse>(
+                data: nil,
+                error: ErrorResponse(details: ErrorDetails(http: "400", msg: "Response Error")),
+                status: "",
+                traceId: ""
+            )
+        )
+        
+        let url = BitLabsRouter.getSurveys(sdk: "").urlRequest!.url!
+        let mock = Mock(url: url, ignoreQuery: true, dataType: .json, statusCode: 200, data: [.get: json])
+        mock.register()
+        
+        
+        let expectation = XCTestExpectation(description: "Error received")
+        bitlabsAPI.getSurveys(sdk: "") { result in
+            switch result {
+            case .success(let surveys):
+                XCTFail("Request succeeded: \(surveys)")
+            case .failure(_):
+                expectation.fulfill()
             }
-            
-            let bitlabsAPI = BitLabsAPI(Session())
-
-            let expectation = XCTestExpectation(description: "Error received")
-            bitlabsAPI.getSurveys(sdk: "") { result in
-                switch result {
-                case .success(let surveys):
-                    XCTFail("Request succeeded: \(surveys)")
-                case .failure(_):
-                    expectation.fulfill()
-                }
-            }
-
-            wait(for: [expectation], timeout: 10.0)
-        } catch(let error) {
-            XCTFail("Failed to parse hardcoded. Error: \(error)")
         }
+        
+        wait(for: [expectation], timeout: 10.0)
     }
     
     func testGetAppSettings_Failure() {
-        stub(condition: forAll()) {_ in
-         return HTTPStubsResponse(error: Exception("Failure Error"))
-        }
-
-        let bitlabsAPI = BitLabsAPI(Session())
-
+        let url = BitLabsRouter.getAppSettings.urlRequest!.url!
+        let mock = Mock(url: url, ignoreQuery: true, dataType: .json, statusCode: 500, data: [.get: Data()], requestError: Exception("Error"))
+        mock.register()
+        
         let expectation = XCTestExpectation(description: "Error received")
         expectation.isInverted = true
+        
         bitlabsAPI.getAppSettings { visual, currency, promotion  in
             
-            XCTFail("Completion block should not be called for erros")
+            XCTFail("Completion block should not be called")
             expectation.fulfill()
         }
-
+        
         wait(for: [expectation], timeout: 10.0)
     }
     
     func testGetAppSettings_Response_Success() {
-        do {
-            let json = try encoder.encode(BitLabsResponse(data: GetAppSettingsResponse(visual: Visual(backgroundColor: "", colorRatingThreshold: 0, customLogoUrl: "", elementBorderRadius: "", hideRewardValue: true, interactionColor: "", navigationColor: "", offerwallWidth: "", screenoutReward: "", surveyIconColor: ""), currency: Currency(floorDecimal: true, factor: "", symbol: Symbol(content: "", isImage: true), currencyPromotion: 0, bonusPercentage: 20), promotion: Promotion(startDate: "", endDate: "", bonusPercentage: 1)), error: nil, status: "", traceId: ""))
-            
-            stub(condition: forAll()) { _ in
-                return HTTPStubsResponse(data: json, statusCode: Int32(200), headers: ["Content-Type": "application/json"])
-            }
-
-            let bitlabsAPI = BitLabsAPI(Session())
-
-            let expectation = XCTestExpectation(description: "Response received")
-            bitlabsAPI.getAppSettings { visual, currency, promotion  in
-                expectation.fulfill()
-            }
-
-            wait(for: [expectation], timeout: 10.0)
-        } catch {
-            XCTFail("Failed to parse hardcoded. Error: \(error)")
+        let json = try! encoder.encode(
+            BitLabsResponse(
+                data: GetAppSettingsResponse(visual: Visual(backgroundColor: "", colorRatingThreshold: 0, customLogoUrl: "", elementBorderRadius: "", hideRewardValue: true, interactionColor: "", navigationColor: "", offerwallWidth: "", screenoutReward: "", surveyIconColor: ""), currency: Currency(floorDecimal: true, factor: "", symbol: Symbol(content: "", isImage: true), currencyPromotion: 0, bonusPercentage: 20), promotion: Promotion(startDate: "", endDate: "", bonusPercentage: 1)),
+                error: nil,
+                status: "",
+                traceId: ""
+            )
+        )
+        
+        let url = BitLabsRouter.getAppSettings.urlRequest!.url!
+        let mock = Mock(url: url, ignoreQuery: true, dataType: .json, statusCode: 200, data: [.get: json])
+        mock.register()
+        
+        let expectation = XCTestExpectation(description: "Response received")
+        bitlabsAPI.getAppSettings { visual, currency, promotion  in
+            XCTAssertTrue(promotion?.bonusPercentage == 1)
+            expectation.fulfill()
         }
+        
+        wait(for: [expectation], timeout: 10.0)
     }
-
+    
     func testGetAppSettings_Response_Error() {
-        do {
-            let json = try encoder.encode(BitLabsResponse<GetSurveysResponse>(data: nil, error: ErrorResponse(details: ErrorDetails(http: "400", msg: "Response Error")), status: "", traceId: ""))
-            
-            stub(condition: forAll()) { _ in
-                return HTTPStubsResponse(data: json, statusCode: Int32(200), headers: ["Content-Type": "application/json"])
-            }
-
-            let bitlabsAPI = BitLabsAPI(Session())
-
-            let expectation = XCTestExpectation(description: "Error received")
-            expectation.isInverted = true
-            
-            bitlabsAPI.getAppSettings { visual, currency, promotion  in
-                expectation.fulfill()
-            }
-
-            wait(for: [expectation], timeout: 10.0)
-        } catch {
-            XCTFail("Failed to parse hardcoded. Error: \(error)")
+        let json = try! encoder.encode(
+            BitLabsResponse<GetSurveysResponse>(
+                data: nil,
+                error: ErrorResponse(details: ErrorDetails(http: "400", msg: "Response Error")),
+                status: "",
+                traceId: ""
+            )
+        )
+        
+        let url = BitLabsRouter.getAppSettings.urlRequest!.url!
+        let mock = Mock(url: url, ignoreQuery: true, dataType: .json, statusCode: 200, data: [.get: json])
+        mock.register()
+        
+        let expectation = XCTestExpectation(description: "Error received")
+        expectation.isInverted = true
+        
+        bitlabsAPI.getAppSettings { visual, currency, promotion  in
+            XCTFail("Completion block should not be called")
+            expectation.fulfill()
         }
+        
+        wait(for: [expectation], timeout: 10.0)
     }
     
     func testGetLeaderboard_Failure() {
-        stub(condition: forAll()) {_ in
-         return HTTPStubsResponse(error: Exception("Failure Error"))
-        }
-
-        let bitlabsAPI = BitLabsAPI(Session())
-
+        let url = BitLabsRouter.getLeaderboard.urlRequest!.url!
+        let mock = Mock(url: url, ignoreQuery: true, dataType: .json, statusCode: 500, data: [.get: Data()], requestError: Exception("Error"))
+        mock.register()
+        
         let expectation = XCTestExpectation(description: "Error received")
         expectation.isInverted = true
         bitlabsAPI.getLeaderboard { _ in
             XCTFail("Completion block should not be called")
             expectation.fulfill()
         }
-
+        
         wait(for: [expectation], timeout: 10.0)
     }
     
     func testGetLeaderboard_Response_Success() {
-        do {
-            let json = try encoder.encode(BitLabsResponse(data: GetLeaderboardResponse(nextResetAt: "", ownUser: User(earningsRaw: 0.0, name: "", rank: 0), rewards: [Reward(rank: 0, rewardRaw: 0.0)], topUsers: nil), error: nil, status: "", traceId: ""))
-            
-            stub(condition: forAll()) { _ in
-                return HTTPStubsResponse(data: json, statusCode: Int32(200), headers: ["Content-Type": "application/json"])
-            }
-
-            let bitlabsAPI = BitLabsAPI(Session())
-
-            let expectation = XCTestExpectation(description: "Response received")
-            bitlabsAPI.getLeaderboard { leaderboard in
-                expectation.fulfill()
-            }
-
-            wait(for: [expectation], timeout: 10.0)
-        } catch {
-            XCTFail("Failed to parse hardcoded. Error: \(error)")
+        let json = try! encoder.encode(
+            BitLabsResponse(
+                data: GetLeaderboardResponse(nextResetAt: "", ownUser: User(earningsRaw: 0.0, name: "", rank: 0), rewards: [Reward(rank: 0, rewardRaw: 0.0)], topUsers: nil),
+                error: nil,
+                status: "",
+                traceId: ""
+            )
+        )
+        
+        let url = BitLabsRouter.getLeaderboard.urlRequest!.url!
+        let mock = Mock(url: url, ignoreQuery: true, dataType: .json, statusCode: 200, data: [.get: json])
+        mock.register()
+        
+        let expectation = XCTestExpectation(description: "Response received")
+        bitlabsAPI.getLeaderboard { leaderboard in
+            expectation.fulfill()
         }
+        
+        wait(for: [expectation], timeout: 10.0)
     }
-
+    
     func testGetLeaderboard_Response_Error() {
-        do {
-            let json = try encoder.encode(BitLabsResponse<GetSurveysResponse>(data: nil, error: ErrorResponse(details: ErrorDetails(http: "400", msg: "Response Error")), status: "", traceId: ""))
-            
-            stub(condition: forAll()) { _ in
-                return HTTPStubsResponse(data: json, statusCode: Int32(200), headers: ["Content-Type": "application/json"])
-            }
-
-            let bitlabsAPI = BitLabsAPI(Session())
-
-            let expectation = XCTestExpectation(description: "Error received")
-            expectation.isInverted = true
-
-            bitlabsAPI.getLeaderboard { _ in
-                XCTFail("Completion block should not be called")
-                expectation.fulfill()
-            }
-
-            wait(for: [expectation], timeout: 10.0)
-        } catch {
-            XCTFail("Failed to parse hardcoded. Error: \(error)")
+        let json = try! encoder.encode(
+            BitLabsResponse<GetSurveysResponse>(
+                data: nil,
+                error: ErrorResponse(details: ErrorDetails(http: "400", msg: "Response Error")),
+                status: "",
+                traceId: ""
+            )
+        )
+        
+        let url = BitLabsRouter.getLeaderboard.urlRequest!.url!
+        let mock = Mock(url: url, ignoreQuery: true, dataType: .json, statusCode: 200, data: [.get: json])
+        mock.register()
+        
+        let expectation = XCTestExpectation(description: "Error received")
+        expectation.isInverted = true
+        bitlabsAPI.getLeaderboard { _ in
+            XCTFail("Completion block should not be called")
+            expectation.fulfill()
         }
+        
+        wait(for: [expectation], timeout: 10.0)
     }
 }
