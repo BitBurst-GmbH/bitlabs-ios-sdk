7e9f50551207f79e8c6c529088bc761ea75617af
Omar <omar.raad@prodege.com>
2025-06-05 14:32:40 +0200

    Migrate Setting to new api request

--- BitLabs/Classes/Shared/Models/BitLabs/GetAppSettingsResponse.swift
+++ BitLabs/Classes/Shared/Models/BitLabs/GetAppSettingsResponse.swift
@@ -5,10 +5,6 @@
 //  Created by Omar Raad on 22.07.22.
 //
 
-import Foundation
-
 struct GetAppSettingsResponse: Codable {
-    let visual: Visual
-    let currency: Currency
-    let promotion: Promotion?
+    let configuration: [Configuration]
 }
